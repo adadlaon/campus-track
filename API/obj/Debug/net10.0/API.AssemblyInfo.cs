e648b2de577c8aef05db97fe033154ac0ce18a2a
adadlaon <adadlaon@hotmail.com>
2026-09-13 21:46:00 -0400

    Initial checkin with users and seed data

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ffa61d16e8d56180ef4ff34817878af6a70ca62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0a396e7f9f81ac7ea7fc1d88ece0101f3b6f674")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
