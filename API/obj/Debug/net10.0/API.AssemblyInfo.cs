8e2f0ae5e94b8b37a611ada69daf9780fc16d746
adadlaon <adadlaon@hotmail.com>
2026-09-21 12:14:12 -0400

    Refactor styles.css to properly format daisyui plugin configuration

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c79d8ffa7544180334afde5cca0fc0adf358cdc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5581e8d4c74d49c8c060bcd75e919b0047863f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
